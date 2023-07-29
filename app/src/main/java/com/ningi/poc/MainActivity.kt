2d92eb2f74187f43aa943b65ba51261a9cc8e058
Terence Baker <terence.baker@lush.co.uk>
2023-07-29 13:47:58 +0100

    Tidied up Splash. Added a top bar.

--- app/src/main/java/com/ningi/poc/MainActivity.kt
+++ app/src/main/java/com/ningi/poc/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ningi.poc.enums.BottomNavigationBarItem
 import com.ningi.poc.ui.composables.screens.MainScreen
 import com.ningi.poc.ui.theme.NingiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
             NingiTheme {
--- app/src/main/java/com/ningi/poc/ui/composables/screens/MainScreen.kt
+++ app/src/main/java/com/ningi/poc/ui/composables/screens/MainScreen.kt
@@ -1,13 +1,28 @@
 package com.ningi.poc.ui.composables.screens
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.ningi.poc.R
 import com.ningi.poc.enums.BottomNavigationBarItem
 import com.ningi.poc.ui.composables.elements.BottomNavigationBar
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
@@ -15,18 +30,44 @@ fun MainScreen(
     items: Array<BottomNavigationBarItem>,
     onItemClick: (BottomNavigationBarItem) -> Unit
 ) {
-    Scaffold(bottomBar = {
-        BottomNavigationBar(
-            selectedItem = selectedItem,
-            items = items,
-            onItemClick = onItemClick
-        )
-    }) {
-        when (selectedItem) {
-            BottomNavigationBarItem.Account -> AccountScreen()
-            BottomNavigationBarItem.Documents -> DocumentsScreen()
-            BottomNavigationBarItem.Chat -> ChatScreen()
-            BottomNavigationBarItem.Settings -> SettingsScreen()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_topbar_foreground),
+                            contentDescription = null,
+                            modifier = Modifier.size(30.dp)
+                        )
+                        Text(text = stringResource(id = selectedItem.text))
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                )
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(
+                selectedItem = selectedItem,
+                items = items,
+                onItemClick = onItemClick
+            )
+        }) {
+        Surface(modifier = Modifier.padding(it)) {
+            when (selectedItem) {
+                BottomNavigationBarItem.Account -> AccountScreen()
+                BottomNavigationBarItem.Documents -> DocumentsScreen()
+                BottomNavigationBarItem.Chat -> ChatScreen()
+                BottomNavigationBarItem.Settings -> SettingsScreen()
+            }
         }
     }
 }
